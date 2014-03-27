e585482a42a9ab4f8e1e534c6a70a116dac9a666
Ben Johnson <bjohnson41287@gmail.com>
2014-03-26 21:00:53 -0700

    initial project repository

---
+++ GramSchmidt_App/src/GramSchmidt.cc
@@ -0,0 +1,57 @@
+/**
+********************************************************************************
+** @file    GramSchmidt.cc
+** @brief   Starting point for the Gram-Schmidt algorithm
+**
+** @details The Gram-Schmidt process generates an orthonormal set of n basis
+**          vectors, given a non-orthogonal set of n vectors that span the
+**          n-dimensional space.
+**
+** @author  Benjamin Johnson
+** @version 1.0
+** @date    Sunday March 23, 2014
+**
+** @copyright Copyright 2014 by Benjamin Johnson
+**            You can freely redistribute and/or modify the contents of this
+**            file under the terms of the GNU General Public License version 3,
+**            or any later versions.
+********************************************************************************
+*/
+
+/*
+********************************************************************************
+**  (C) Copyright 2014 by Benjamin Johnson
+**
+**  GramSchmidt is free software: you can redistribute it and/or modify it under
+**  the terms of the GNU General Public License as published by the Free
+**  Software Foundation, either version 3 of the License, or (at your option)
+**  any later version.
+**
+**  GramSchmidt is distributed in the hope that it will be useful, but WITHOUT
+**  ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY or
+**  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+**  more details.
+**
+**  You should have received a copy of the GNU General Public License along
+**  with GramSchmidt. If not, see <http://www.gnu.org/licenses/>.
+********************************************************************************
+*/
+
+/*------------------------------[Include Files]-------------------------------*/
+#include <stdio.h>
+
+/*-------------------------------[Begin Code]---------------------------------*/
+/**
+********************************************************************************
+** Function: main
+** @brief   This is the main() function for the start of the GramSchmidt C++
+**          program.
+** @param   argc    Number of program input arguments
+** @param   argv[]  Array of char pointers to the input arguments
+** @return  int
+********************************************************************************
+*/
+int main(int argc, char* argv[])
+{
+    printf("This is the GramSchmidt program.\n");
+}
