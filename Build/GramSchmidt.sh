1b7be15f5281cbdf07c4cd77ea4ec60375a62a38
Ben Johnson <bjohnson41287@gmail.com>
2014-11-13 21:01:50 -0700

    Capability added to generate release tarballs of annotated tag versions
    Release tarballs can now be created by passing the 'release' argument with an
    optional name or SHA-1 hash of an annotated tag that exists in the remote and
    local repositories.
    
    File: Build/GramSchmidt.sh
    --------------------------
    - The 'release' option was added to generate release tarballs of commits pointed
      to by specified annotated tags.
    - If an annotated tag is not specified, the most current annotated tag in the
      local repository is found and the user is prompted for whether or not they
      would like to continue generating a release tarball with this version.
    - If a lightweight tag is specified, an error is thrown that lets the user know
      the tag and commit SHA-1 are identical and the release tarball is not created.
    
    File: .gitignore
    ----------------
    - Since the release tarballs are placed in a "release" directory off of the
      PROJ_ROOT_PATH directory, this "release" directory has been added to the list
      of directories that will not be included in the repository

--- Build/GramSchmidt.sh
+++ Build/GramSchmidt.sh
@@ -5,7 +5,7 @@
 # Author: Benjamin Johnson
 #
 # Date Created: Monday September 29, 2014
-# Date Revised: Saturday October 25, 2014
+# Date Revised: Thursday November 13, 2014
 #
 # Description: Bash script used to compile the GramSchmidt program
 ################################################################################
@@ -19,24 +19,66 @@ buildOutLog=build_stdout.log
 configErrLog=configure_stderr.log
 buildErrLog=build_stderr.log
 
-
 #
 # Manual pages for usage instructions
 #
 build_man ()
 {
     echo ""
-    echo "Valid Input Arguments:"
-    echo "    setenv    - Set needed environment variables"
-    echo "    configure - Prepare the project directories for compilation"
-    echo "    build     - Configure and compile the project"
-    echo "    distclean - Delete all files created by the configure and/or"
-    echo "                build steps"
+    echo -e "\033[1mSYNOPSIS\033[0m"
+    echo -e "\tGramSchmidt.sh [OPTIONS] [TAG]"
+    echo ""
+    echo -e "\033[1mOPTIONS\033[0m"
+    echo -e "\tsetenv"
+    echo -e "\t    Set the environment for the GramSchmidt project."
+    echo ""
+    echo -e "\tconfigure"
+    echo -en "\t    Prepare the project directories for compilation of the "
+    echo "project source code."
+    echo ""
+    echo -e "\tbuild"
+    echo -e "\t    Configure and compile the project source code."
+    echo ""
+    echo -e "\tdistclean"
+    echo -en "\t    Delete all files created by the \033[4mconfigure\033[0m "
+    echo -e "and \033[4mbuild\033[0m steps."
+    echo ""
+    echo -e "\trelease"
+    echo -en "\t    Create a release tarball of the commit pointed to by "
+    echo -en "\033[4mTAG\033[0m. \033[4mTAG\033[0m can either be the name or "
+    echo -e "the SHA-1 hash of an annotated tag."
+    echo -en "\t    If a lightweight tag is given, an error will be generated "
+    echo -en "saying the SHA-1 of the tag and commit are the same. If "
+    echo -e "\033[4mTAG\033[0m is not"
+    echo -en "\t    specified, then the most current annotated tag is found in "
+    echo "the local repository and the user is prompted for whether or not"
+    echo -en "\t    they would like to create a release tarball for this tag "
+    echo "version."
+    echo ""
+    echo -en "\t    A remote repository must also exist and will be searched "
+    echo -en "for the same \033[4mTAG\033[0m value. If a remote is not found "
+    echo "or the specified"
+    echo -en "\t    tag is not found in the remote repository, then the "
+    echo -n "release process will generate an error and the tarball will not "
+    echo "be created."
     echo ""
 }
 
 #
-# Check to ensure the environment variables are set
+# Ensure the correct number of input arguments were given
+#
+checkInputs ()
+{
+    goodInputs=true
+    if [ ${noOfArgs} -gt $2 ]; then
+        echo "Incorrect number of arguments for '$1'"
+        echo "Run this script without any inputs for appropriate use."
+        goodInputs=false
+    fi
+}
+
+#
+# Ensure the environment variables are set
 #
 checkProjEnv ()
 {
@@ -52,8 +94,9 @@ checkProjEnv ()
 #
 # Check the number of input arguments
 #
-if [ $# -ne 1 ]; then
-    echo "Invalid number of arguments"
+noOfArgs=$#
+if [ $# -eq 0 ]; then
+    build_man
     return 1
 fi
 
@@ -61,6 +104,14 @@ fi
 # Set the necessary environment variables
 #
 if [ "$1" = "setenv" ]; then
+    #
+    # Ensure the inputs are correct and the environment variables are set
+    #
+    checkInputs $1 1
+    if [ ${goodInputs} = false ]; then
+        return 1
+    fi
+
     echo "Setting environment"
 
     #
@@ -71,10 +122,12 @@ if [ "$1" = "setenv" ]; then
     export STD_MAKE_PATH=Build/StdMake
 
     #
-    # Link the repository git hooks to the .git/hooks directory
+    # Link the repository git hooks to the .git/hooks directory, if it exists
     #
-    echo -en "\n  Linking git hooks to the .git/hooks directory\n"
-    ${PROJ_ROOT_PATH}/Tools/Scripts/LinkGitHooks.sh
+    if [ -d ${PROJ_ROOT_PATH}/.git ]; then
+        echo -en "\n  Linking git hooks to the .git/hooks directory\n"
+        ${PROJ_ROOT_PATH}/Tools/Scripts/LinkGitHooks.sh
+    fi
 
     #
     # Let the user know what PROJ_ROOT_PATH is defined as
@@ -86,14 +139,19 @@ if [ "$1" = "setenv" ]; then
 #
 elif [ "$1" = "configure" ]; then
     #
-    # Ensure the environment variables are set
+    # Ensure the inputs are correct and the environment variables are set
     #
+    checkInputs $1 1
+    if [ ${goodInputs} = false ]; then
+        return 1
+    fi
+
     checkProjEnv
     if [ ${projEnvSet} = false ]; then
         return 1
     fi
 
-    echo "Configuring GramSchmidt..."
+    echo "Configuring GramSchmidt"
     make -C ${PROJ_ROOT_PATH} configure
 
 #
@@ -101,8 +159,13 @@ elif [ "$1" = "configure" ]; then
 #
 elif [ "$1" = "build" ]; then
     #
-    # Ensure the environment variables are set
+    # Ensure the inputs are correct and the environment variables are set
     #
+    checkInputs $1 1
+    if [ ${goodInputs} = false ]; then
+        return 1
+    fi
+
     checkProjEnv
     if [ ${projEnvSet} = false ]; then
         return 1
@@ -112,10 +175,10 @@ elif [ "$1" = "build" ]; then
     # Configure and build the project. This ensures everything is up to date for
     # make.
     #
-    echo "Configuring GramSchmidt..."
+    echo "Configuring GramSchmidt"
     make -C ${PROJ_ROOT_PATH} configure
 
-    echo "Building GramSchmidt..."
+    echo "Building GramSchmidt"
     make -C ${PROJ_ROOT_PATH} all
 
 #
@@ -123,8 +186,13 @@ elif [ "$1" = "build" ]; then
 #
 elif [ "$1" = "distclean" ]; then
     #
-    # Ensure the environment variables are set
+    # Ensure the inputs are correct and the environment variables are set
     #
+    checkInputs $1 1
+    if [ ${goodInputs} = false ]; then
+        return 1
+    fi
+
     checkProjEnv
     if [ ${projEnvSet} = false ]; then
         return 1
@@ -132,26 +200,175 @@ elif [ "$1" = "distclean" ]; then
 
     make -C ${PROJ_ROOT_PATH} distclean
 
+#
+# Generate a release tarball of the commit snapshot pointed to by the specified
+# annotated tag name
+#
+elif [ "$1" = "release" ]; then
+    #
+    # Ensure the inputs are correct and the environment variables are set
+    #
+    checkInputs $1 2
+    if [ ${goodInputs} = false ]; then
+        return 1
+    fi
+
+    checkProjEnv
+    if [ ${projEnvSet} = false ]; then
+        return 1
+    fi
+
+    #
+    # Check the tag name argument to ensure it is a valid annotated tag name
+    #
+    if [ -d ${PROJ_ROOT_PATH}/.git ]; then
+        if [ -z "$2" ]; then
+            echo -e "Tag name not specified."
+            tagName=`git describe`
+            echo -n "Use the most current annotated tag ${tagName}? (y/n): "
+            read useTagAns
+
+            case "${useTagAns}" in
+                y | yes)
+                    ;;
+                n | no)
+                    echo -e "Aborting release"
+                    return 1
+                    ;;
+                *)
+                    echo -e "ERROR: Unknown input \"${useTagAns}\""
+                    echo "Aborting release"
+                    return 1
+                    ;;
+            esac
+
+        else
+            tagName="$2"
+        fi
+
+        if [ ! -z ${tagName} ]; then
+            #
+            # Grab the unique remote URLs to the current repository and ask the
+            # user which remote should be selected. If only one remote is found,
+            # do not prompt the user.
+            #
+            remoteURL=(`git remote | git ls-remote --get-url`)
+            if [ ${#remoteURL[@]} -eq 0 ]; then
+                echo "ERROR: No remote repository found. A remote repository"
+                echo "must exist to validate release versions."
+                return 1
+
+            elif [ ${#remoteURL[@]} -gt 1 ]; then
+                echo -e "\nRemote Repositories"
+                echo "-------------------"
+
+                for ((i=0; i<${#remoteURL[@]}; i++)); do
+                    echo -e "$(($i+1)))\t${remoteURL[$i]}"
+                done
+
+                echo -en "\nRepository to read annotated tags: "
+                read URLnum
+
+                #
+                # Check for a valid input. Bash complains if the input is not an
+                # integer, since it expects it in the comparison below.
+                #
+                if [[ ! ${URLnum} =~ ^[0-9]+$ ]] || \
+                   [ ${URLnum} -lt 1 ] || \
+                   [ ${URLnum} -gt ${#remoteURL[@]} ]; then
+                    echo "ERROR: Input ${URLnum} is not a valid option"
+                    return 1
+                else
+                    repoURL=${remoteURL[$((${URLnum}-1))]}
+                fi
+            else
+                repoURL=${remoteURL[0]}
+            fi
+
+            #
+            # Check the local repository for the same tag and commit hash values
+            # as the remote repository
+            #
+            localTagHash=`git show-ref --tags ${tagName} | awk '{print $1}'`
+            if [ -z ${localTagHash} ]; then
+                echo -n "ERROR: Tag ${tagName} does not exist in the local "
+                echo "repository."
+                return 1
+            fi
+
+            echo -e "Accessing annotated tags from repository: ${repoURL}\n"
+
+            tagRegExp="${tagName}$|${tagName}\^\{\}$"
+            rmtTagRefs=(`git ls-remote --tags ${repoURL} | \
+                         grep -E "${tagRegExp}" | awk '{print $1}'`)
+            rmtTagHash=${rmtTagRefs[0]}
+            rmtCmtHash=${rmtTagRefs[1]}
+
+            if [ "${rmtTagHash}" = "${rmtCmtHash}" ]; then
+                echo "ERROR: Specified tag ${tagName} is not an annotated tag"
+                echo "       in the remote repository."
+                return 1
+            fi
+
+            if [ "${localTagHash}" != "${rmtTagHash}" ]; then
+                echo "ERROR: Local tag ${tagName} SHA-1 hash does not match the"
+                echo "       repository tag ${tagName} SHA-1 hash. Either"
+                echo "       update the remote or local repository or specify"
+                echo "       another tag."
+                return 1
+            fi
+
+            localCmtHash=`git log --format=%H -1 ${localTagHash}`
+
+            if [ "${localTagHash}" = "${localCmtHash}" ]; then
+                echo "ERROR: Specified tag ${tagName} is not an annotated tag"
+                echo "       in the local repository."
+                return 1
+            fi
+
+            if [ "${rmtCmtHash}" != "${localCmtHash}" ]; then
+                echo "ERROR: Specified tag ${tagName} points to different"
+                echo "       commit snapshots in remote and local repositories."
+                echo "       Either update the remote or local repository or"
+                echo "       specify another tag."
+                return 1
+            fi
+
+            #
+            # Create the release tarball
+            #
+            tagDir=`echo ${tagName} | sed "s|\.|_|"`
+            relDir="${PROJ_ROOT_PATH}/release/${tagDir}"
+            archPrefix="`basename ${PROJ_ROOT_PATH}`/"
+            archName="`basename ${PROJ_ROOT_PATH}`_${tagDir}.tar.gz"
+
+            if [ ! -d ${relDir} ]; then
+                mkdir -p ${relDir}
+            fi
+
+            git archive ${localTagHash} --prefix="${archPrefix}" | \
+                gzip > "${relDir}/${archName}"
+
+            echo "Generated release tarball:"
+            echo "${relDir}/${archName}"
+        else
+            echo "ERROR: No annotated tags found in the local project."
+            echo "       You must create one and push it to the remote"
+            echo "       repository before a release tarball can be created."
+        fi
+    else
+        echo "ERROR"
+        echo "${PROJ_ROOT_PATH}/.git directory not found."
+        echo "Use the 'tar' command instead to generate an archive."
+    fi
+
 else
     #
     # An invalid argument has been input. Print the manual pages and exit.
     #
     echo "Invalid argument: $1"
-    build_man
+    echo "Run this script without any inputs for appropriate use."
     return 1
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # End GramSchmidt.sh
